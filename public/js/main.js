e2b5c78595b2bf0c7d0b5dc86e368e7ed95db4da
Melina1996 <melina.schlachter@gmx.de>
2024-01-19 11:46:01 +0100

    finished JS functions & changed HTML Divs to JS LOOP

--- public/js/main.js
+++ public/js/main.js
@@ -12,19 +12,32 @@ let containerSquares = document.querySelector("#my-container")
 //all the colors with effect
 
 
-let blue = ["box-shadow: 0px 0px 5px 1px #3286c0;", "background-color: #3286c0;"]
+let blue = ["box-shadow: 0px 0px 5px 3px #3286c0;", "background-color: #3286c0;"]
 
-let green = ["box-shadow: 0px 0px 5px 1px #3b7a4b;", "background-color: #3b7a4b;"]
+let green = ["box-shadow: 0px 0px 5px 3px #3b7a4b;", "background-color: #3b7a4b;"]
 
-let lila = ["box-shadow: 0px 0px 5px 1px #8d43ae;", "background-color:#8d43ae;"]
+let lila = ["box-shadow: 0px 0px 5px 3px #8d43ae;", "background-color:#8d43ae;"]
 
-let red = ["box-shadow: 0px 0px 5px 1px #ba4134;","background-color:#ba4134;"]
+let red = ["box-shadow: 0px 0px 5px 3px #ba4134;","background-color:#ba4134;"]
 
-let orange = ["box-shadow: 0px 0px 5px 1px #c76f29;","background-color:#c76f29;"]
+let orange = ["box-shadow: 0px 0px 5px 3px #c76f29;","background-color:#c76f29;"]
 
 let allColors = [blue,green,lila,red,orange]
 
 console.log(containerSquares)
+
+//create my Divs
+
+let mySquare
+
+//I want to create 405 Divs (like this I have my container filled)
+
+for (let i = 0; i <= 405; i++) {
+  mySquare = document.createElement("div") 
+  mySquare.setAttribute("class","my-square")
+  containerSquares.appendChild(mySquare)
+}
+
 
 function getRandomStyle(e){
       index = Math.floor(Math.random()*allColors.length)
@@ -33,8 +46,15 @@ function getRandomStyle(e){
       console.log(style)
       if(e.target.id != "my-container"){
         e.target.style = style[0]
-        e.target.style = style[1]   
+        e.target.style = style[1]  
+        e.target.classList.add("someClass")
+        console.log(e.target)
       }
+      setInterval(function() {
+        e.target.style.boxShadow = "" 
+        e.target.style.backgroundColor = ""
+        e.target.classList.remove("someClass")
+        }, 2000);
 }
 
 containerSquares.addEventListener("mouseover",getRandomStyle)
