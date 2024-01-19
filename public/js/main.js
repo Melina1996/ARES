287c0add3dbc2fe0b6276cd7a52b679cb3a2a977
Melina1996 <melina.schlachter@gmx.de>
2024-01-19 10:59:41 +0100

    added JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,47 @@
-//variables for each of my h2
+//variable for my container with all the squares inside
 
-let allSocialMedia = document.querySelectorAll(".each-logo span")
+let containerSquares = document.querySelector("#my-container")
 
-allSocialMedia.forEach(element => {
-  element.innerText = "0";
+// containerSquares.forEach(element => {
+//   element.addEventListener("mouseover",()=>{
+//   console.log(element)
+//   })
+// });
 
-  function counting(){
-    let maxCount = +element.getAttribute("data-target")
-    let myH2Count = +element.innerText
 
-    let increment = maxCount/200 //will increment by 200er-steps
+//all the colors with effect
 
-    if(myH2Count < maxCount){
-      element.innerText = Math.round(myH2Count + increment);
-      setTimeout(counting,1)
-    } else {
-      element.innerText = maxCount
-    }
-  }
-  counting()
-  })
+
+let blue = ["box-shadow: 0px 0px 5px 1px #3286c0;", "background-color: #3286c0;"]
+
+let green = ["box-shadow: 0px 0px 5px 1px #3b7a4b;", "background-color: #3b7a4b;"]
+
+let lila = ["box-shadow: 0px 0px 5px 1px #8d43ae;", "background-color:#8d43ae;"]
+
+let red = ["box-shadow: 0px 0px 5px 1px #ba4134;","background-color:#ba4134;"]
+
+let orange = ["box-shadow: 0px 0px 5px 1px #c76f29;","background-color:#c76f29;"]
+
+let allColors = [blue,green,lila,red,orange]
+
+console.log(containerSquares)
+
+function getRandomStyle(e){
+      index = Math.floor(Math.random()*allColors.length)
+      console.log(index);
+      style = allColors[index]
+      console.log(style)
+      if(e.target.id != "my-container"){
+        e.target.style = style[0]
+        e.target.style = style[1]   
+      }
+}
+
+containerSquares.addEventListener("mouseover",getRandomStyle)
+
+
+
+
 
 
 
