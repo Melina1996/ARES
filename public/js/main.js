74301efd687c3fee1935c48de28e12ad2da483bd
Melina1996 <melina.schlachter@gmx.de>
2024-01-21 18:27:57 +0100

    added glowing effect

--- public/js/main.js
+++ public/js/main.js
@@ -14,16 +14,16 @@ for (let i = 0; i <= 407; i++) {
 }
 
 
-//stock the style-properties of my colors into variables
-let blue = ["box-shadow: 0px 0px 50px 10px #3286c0;", "background-color: #3286c0;"]
+//stock the style-property background of my colors into variables
+let blue = ["background-color: #3286c0;"]
 
-let green = ["0px 0px 105px 45px rgba(45,255,196,0.9);", "background-color: #3b7a4b;"]
+let green = ["background-color: #3b7a4b;"]
 
-let lila = ["box-shadow: 0px 0px 50px 10px #8d43ae;", "background-color:#8d43ae;"]
+let lila = ["background-color: #8d43ae;"]
 
-let red = ["box-shadow: 0px 0px 50px 10px #ba4134;","background-color:#ba4134;"]
+let red = ["background-color: #ba4134;"]
 
-let orange = ["box-shadow: 0px 0px 50px 10px #c76f29;","background-color:#c76f29;"]
+let orange = ["background-color: #c76f29;"]
 
 //all my colors in an array
 let allColors = [blue,green,lila,red,orange]
@@ -35,22 +35,40 @@ let style
 function getRandomStyle(e){
       //random index to select a random color
       index = Math.floor(Math.random()*allColors.length)
-      console.log(index);
       //my random color
       style = allColors[index]
       //if targeted element is not container (because i dont want to color the container)
       if(e.target.id != "my-container"){
-        e.target.style = style[0] //add my first style-element
-        e.target.style = style[1] //add my first style
+        e.target.style = style[0] //add my background-style-element
+
+        //tried to add box-shadow to my arraw but couldn't be read...
+        switch (style[0]) {
+          case "background-color: #3286c0;":
+            e.target.style.boxShadow = "0px 0px 50px 3px #3286c0"
+            break;
+          case "background-color: #3b7a4b;":
+            e.target.style.boxShadow = "0px 0px 50px 3px #3b7a4b"
+            break;
+          case "background-color: #8d43ae;":
+            e.target.style.boxShadow = "0px 0px 50px 3px #8d43ae"
+            break;
+          case "background-color: #ba4134;":
+            e.target.style.boxShadow = "0px 0px 50px 3px #ba4134;"
+            break;
+          case "background-color: #c76f29;":
+            e.target.style.boxShadow = "0px 0px 50px 3px #c76f29;"
+            break;
+          default:
+            break;
+        }
         //add class with fading animation
-        e.target.classList.add("someClass")
-        console.log(e.target)
+        e.target.classList.add("fading")
       }
       //after 2sec, the boxe's property are set to "" so that it can be targeted again
       setInterval(function() {
         e.target.style.boxShadow = "" 
         e.target.style.backgroundColor = ""
-        e.target.classList.remove("someClass")
+        e.target.classList.remove("fading")
         }, 2000);
 }
 
